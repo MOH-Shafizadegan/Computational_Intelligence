9f641ec349f3bba105268e9b55b5b62db2d8dba0
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2023-11-02 11:58:50 +0330

    Question 3 implemented

--- Coding_Assignments/1/code/Q3.m
+++ Coding_Assignments/1/code/Q3.m
@@ -26,120 +26,120 @@ scatter_features(setosa_data, versicolor_data, virginica_data, 2, 3, 4);
 subplot(2,2,4);
 scatter_features(setosa_data, versicolor_data, virginica_data, 1, 3, 4);
 
+% => use features 1 , 3 , 4
 
-%%
+%%  train a TLU for separating class 1 and class 2
+
 clc; close all;
 
+selected_features = [1, 3, 4];
+
 P = randperm (50);
-train_data = [setosa_data(P(1:40), 3:4) ; versicolor_data(P(1:40), 3:4)];
+train_data = [setosa_data(P(1:40), selected_features) ; ...
+              versicolor_data(P(1:40), selected_features)];
 
 % all data labels within an array (first class 1 then class 0)
 out = [ones(1,40), zeros(1,40)];
 
-% online learning
-% train the model
 n_iter = 100;
 eta = 0.5;
 
-[w_online, theta_online, iter_num] = online_learning(eta, n_iter, train_data, out);
+[w_12, theta_12, iter_num] = online_learning([0; 0; 0], 0, eta, n_iter, train_data, out);
 
-disp('Online learning');
+disp('Separating class 1 and 2');
 fprintf('number of iterations: %d \n', iter_num)
-fprintf('the resulting weigths: w1=%d , w2=%d \n', w_online(1, end), w_online(2, end));
-fprintf('threshold = %d \n', theta_online(end));
+fprintf('the resulting weigths: w1=%d , w2=%d , w3=%d\n', w_12(1, end), ...
+        w_12(2, end), w_12(3, end));
+fprintf('threshold = %d \n', theta_12(end));
 
 figure;
-scatter_features(setosa_data, versicolor_data, 3,4); hold on;
-title('Online learning')
-plot_line(w_online(1, end), w_online(2, end), theta_online(end)); hold off;
+scatter_features(setosa_data, versicolor_data, virginica_data, 1, 3, 4);
+hold on;
+plot_plane(w_12(1, end), w_12(2, end), w_12(2, end), theta_12(end))
 
-%% batch learning
+%% test accuracy for class 1 and 2
 
-n_iter = 100;
-eta = 0.5;
+test_data = [setosa_data(P(41:50), selected_features) ; ...
+              versicolor_data(P(41:50), selected_features)];
+          
+out_test = [ones(1,10), zeros(1,10)];
 
-[w_batch, theta_batch, n_iter_batch] = batch_learning(eta, n_iter, train_data, out);
+y_train_12 = w_12(:, end)' * train_data' >= theta_12(end); 
+y_test_12 = w_12(:, end)' * test_data' >= theta_12(end);
 
-disp('Batch learning');
-fprintf('number of iterations: %d \n', n_iter_batch)
-fprintf('the resulting weigths: w1=%d , w2=%d \n', w_batch(1, end), w_batch(2, end));
-fprintf('threshold = %d \n', theta_batch(end));
+acc_12_test = sum(y_test_12 == out_test)/length(out_test) * 100;
+acc_12_train = sum(y_train_12 == out)/length(out) * 100;
+
+fprintf('Accuracy of classification of class 1 and 2 on test data: %d  \n', acc_12_test);
+fprintf('Accuracy of classification of class 1 and 2 on train data: %d  \n', acc_12_train);
+
+%% train a TLU for separating class 2 and class 3
+
+P = randperm (50);
+train_data = [versicolor_data(P(1:40), selected_features) ; ...
+              virginica_data(P(1:40), selected_features)];
+
+% all data labels within an array (first class 1 then class 0)
+out = [ones(1,40), zeros(1,40)];
+
+eta = 0.01;
+n_iter = 1000;
+
+[w_23, theta_23, n_iter_23] = online_learning([0; 0; 0], 0, eta, n_iter, train_data, out);
+
+disp('Separating class 2 and 3');
+fprintf('number of iterations: %d \n', n_iter_23)
+fprintf('the resulting weigths: w1=%d , w2=%d , w3=%d\n', w_23(1, end), ...
+        w_23(2, end), w_23(3, end));
+fprintf('threshold = %d \n', theta_23(end));
 
 figure;
-scatter_features(setosa_data, versicolor_data, 3,4); hold on;
-title('Batch learning')
-plot_line(w_batch(1, end), w_batch(2, end), theta_batch(end)); hold off;
+scatter_features(setosa_data, versicolor_data, virginica_data, 1, 3, 4);
+hold on;
+plot_plane(w_23(1, end), w_23(2, end), w_23(2, end), theta_23(end))
 
+%% test accuracy for class 2 and 3
 
-%% 
+test_data = [versicolor_data(P(41:50), selected_features) ; ...
+              virginica_data(P(41:50), selected_features)];
+          
+out_test = [ones(1,10), zeros(1,10)];
 
-% online learning w and theta plots
-figure;
-subplot(3,1,1)
-plot(w_online(1,:))
-title('Online learning')
-xlabel('iteration')
-ylabel('w1');
-subplot(3,1,2)
-plot(w_online(2,:))
-xlabel('iteration')
-ylabel('w2');
-subplot(3,1,3)
-plot(theta_online)
-xlabel('iteration')
-ylabel('theta');
+y_train_23 = w_23(:, end)' * train_data' >= theta_23(end); 
+y_test_23 = w_23(:, end)' * test_data' >= theta_23(end);
 
-% batch learning w and theta plots
-figure;
-subplot(3,1,1)
-plot(w_batch(1,:))
-title('Batch learning')
-xlabel('epoch')
-ylabel('w1');
-subplot(3,1,2)
-plot(w_batch(2,:))
-xlabel('epoch')
-ylabel('w2');
-subplot(3,1,3)
-plot(theta_batch)
-xlabel('epoch')
-ylabel('theta');
+acc_23_test = sum(y_test_23 == out_test)/length(out_test) * 100;
+acc_23_train = sum(y_train_23 == out)/length(out) * 100;
+
+fprintf('Accuracy of classification of class 2 and 3 on test data: %d  \n', acc_23_test);
+fprintf('Accuracy of classification of class 2 and 3 on train data: %d  \n', acc_23_train);
 
 %%
 
-test_data = [setosa_data(P(41:50), 3:4) ; versicolor_data(P(41:50), 3:4)];
-out_test = [ones(1,10), zeros(1,10)];
+figure;
+scatter_features(setosa_data, versicolor_data, virginica_data, 1, 3, 4);
+hold on;
+plot_plane(w_12(1, end), w_12(2, end), w_12(2, end), theta_12(end))
+plot_plane(w_23(1, end), w_23(2, end), w_23(2, end), theta_23(end))
 
-y_online = w_online(:, end)' * test_data' >= theta_online(end);
-
-y_batch = w_batch(:, end)' * test_data' >= theta_batch(end);
-
-acc_online = sum(y_online == out_test)/length(out_test) * 100;
-acc_batch = sum(y_batch == out_test)/length(out_test) * 100;
-
-fprintf('Accuracy of classification train by online learning: %d  \n', acc_online);
-fprintf('Accuracy of classification train by batch learning: %d \n', acc_batch);
 
 %% 
 
 function scatter_features(data1, data2, data3, f1, f2, f3)
-    % Plotting features: sepal length vs sepal width
     scatter3(data1(:,f1), data1(:,f2), data1(:,f3), 'filled', 'MarkerFaceColor', 'r');
     hold on;
     scatter3(data2(:,f1), data2(:,f2), data2(:,f3), 'filled', 'MarkerFaceColor', 'g');
     scatter3(data3(:,f1), data3(:,f2), data3(:,f3), 'filled', 'MarkerFaceColor', 'b');
-%     xlabel(sprintf('feature %d', f1));
-%     ylabel(sprintf('feature %d', f2));
-%     title(sprintf('feature %d vs. feature %d', f1, f2));
+    xlabel(sprintf('feature %d', f1));
+    ylabel(sprintf('feature %d', f2));
+    zlabel(sprintf('feature %d', f3));
+    title(sprintf('feature %d vs. feature %d vs. feature %d', f1, f2, f3));
     legend('Iris-setosa', 'Iris-versicolor', 'Iris-virginica');
     hold off;
 end
 
-function [w_array, theta_array, iter_num] = online_learning(eta, n_iter, x, out)
+function [w_array, theta_array, iter_num] = online_learning(init_w, init_theta, eta, n_iter, x, out)
     
-    init_w = [0; 0];
-    init_theta = 5;
-
     w = init_w;
     theta = init_theta;
 
@@ -165,11 +165,8 @@ function [w_array, theta_array, iter_num] = online_learning(eta, n_iter, x, out)
     iter_num = i;
 end
 
-function [w_array, theta_array, iter_num] = batch_learning(eta, n_iter, x, out)
+function [w_array, theta_array, iter_num] = batch_learning(init_w, init_theta, eta, n_iter, x, out)
     
-    init_w = [0; 0];
-    init_theta = 5;
-
     w = init_w;
     theta = init_theta;
 
@@ -199,12 +196,15 @@ function [w_array, theta_array, iter_num] = batch_learning(eta, n_iter, x, out)
     iter_num = i;
 end
 
-function plot_line(w1, w2, theta)
-    slope = -w1/w2;
-    intercept = theta/w2;
+function plot_plane(w1, w2, w3, theta)
+    % Inputs: w1, w2, w3 are the weights of the plane equation: w1*x + w2*y + w3*z = theta
+    
+    % Create a grid of x and y values
+    [x, y] = meshgrid(0:10, 0:10);
 
-    x = 0:0.1:5;
-    y = slope*x + intercept;
+    % Solve for z values using the plane equation
+    z = (theta - w1*x - w2*y) / w3;
 
-    plot(x,y,'r'); xlabel('x'); ylabel('y');
+    % Plot the plane using surf function
+    surf(x, y, z);
 end
